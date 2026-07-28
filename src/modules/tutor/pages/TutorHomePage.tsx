fa13e35ea2dcd23aa0690f73060ec47316e3ef07
jsalvucci <jsalvucci@frvm.utn.edu.ar>
2026-07-28 10:25:34 -0300

    refactor clean home

--- src/modules/tutor/pages/TutorHomePage.tsx
+++ src/modules/tutor/pages/TutorHomePage.tsx
@@ -1,7 +1,6 @@
 import { Box } from "@mui/material";
-import AddSubjectButton from "../components/AddSubjectButton";
 
 export default function TutorHomePage() {
-  return(<><Box sx={{ minHeight: "100vh" }} /><AddSubjectButton /></>)
+  return(<><Box sx={{ minHeight: "100vh" }} /></>)
   ;
 }
